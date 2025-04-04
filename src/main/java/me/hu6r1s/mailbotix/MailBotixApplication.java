6733f6ad68c371b064fb49131e5c8ea3b4887cae
hu6r1s <darmaa333@gmail.com>
2025-04-04 17:12:41 +0900

    git init

---
+++ src/main/java/me/hu6r1s/mailbotix/MailBotixApplication.java
@@ -0,0 +1,13 @@
+package me.hu6r1s.mailbotix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailBotixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailBotixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hu6r1s/mailbotix/MailBotixApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hu6r1s.mailbotix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailBotixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
