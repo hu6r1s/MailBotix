e6737667fe1bbdfff988d5b05ed735e0d3b47a4c
hu6r1s <darmaa333@gmail.com>
2025-04-15 17:08:59 +0900

    fix(Reply): cors and post method

--- src/main/java/me/hu6r1s/mailbotix/domain/reply/controller/ReplyController.java
+++ src/main/java/me/hu6r1s/mailbotix/domain/reply/controller/ReplyController.java
@@ -3,7 +3,8 @@ package me.hu6r1s.mailbotix.domain.reply.controller;
 import lombok.RequiredArgsConstructor;
 import me.hu6r1s.mailbotix.domain.reply.dto.request.ReplyGenerationRequest;
 import me.hu6r1s.mailbotix.domain.reply.service.ReplyService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reply")
+@CrossOrigin(origins = "${frontend.url}", allowCredentials = "true")
 public class ReplyController {
 
   private final ReplyService replyService;
 
-  @GetMapping("/")
+  @PostMapping()
   public String generateEmailReply(@RequestBody ReplyGenerationRequest replyGenerationRequest) {
     return replyService.generateEmailReply(replyGenerationRequest);
   }
